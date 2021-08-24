03e8ede021840e68691d0d87bf5c9c714def66b5
Robert <robsahakyan54@gmail.com>
2021-08-25 00:43:44 +0400

    New Homeworks

---
+++ js/evenOrOdd.js
@@ -0,0 +1,13 @@
+function evenOrOdd(x)
+{
+    if (x % 2 == 0)
+    {
+        console.log("The number is even");
+    }
+    else
+    {
+        console.log("The number is odd");
+    }
+}
+let x = prompt("Enter a number");
+evenOrOdd(x);
